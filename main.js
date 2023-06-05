9655a63ad52f949aa53e84539f2dc4b6d7bfc027
Kiw7 <hanz0704@gmail.com>
2023-06-05 15:58:48 +0700

    add contact

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ function toggleDetail(e) {
   //   console.log(detail);
   $(detail).slideToggle();
 }
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = $("#inp_email");
+  const subject = $("#inp_subject");
+  const message = $("#inp_message");
+
+  if (!$(email).val()) {
+    alert("Email is required.");
+  } else if (!$(subject).val()) {
+    alert("Subject is required.");
+  } else if (!$(message).val()) {
+    alert("Message is required.");
+  } else {
+    alert("Form submitted");
+    $("inp_email").val("");
+    $("inp_subject").val("");
+    $("inp_message").val("");
+  }
+}
